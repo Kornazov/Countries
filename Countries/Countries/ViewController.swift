185de62d68a1ce091193ce6414ffa4c714cce68e
Kristiyan Kornazov <kristiyankornazov@Kristiyans-MacBook-Pro.local>
2023-03-14 17:59:09 +0200

    Create project

---
+++ Countries/Countries/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  Countries
+//
+//  Created by Kristiyan Kornazov on 14.03.23.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
